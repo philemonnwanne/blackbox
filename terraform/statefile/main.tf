de0c771a42326470006ee09dea13917e6deb5659
philemonnwanne <philemonnwanne@gmail.com>
2023-06-24 00:35:28 +0100

    update file

--- terraform/statefile/main.tf
+++ terraform/statefile/main.tf
@@ -64,7 +64,7 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
 
 # connect to a DynamoDB table to use for locking
 resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "vacation-vibe-locks"
+  name         = "vacation-vibe-state-lock"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
