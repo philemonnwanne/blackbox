1467c94d60c598c95afbc8400e36d5b11775aaa7
philemonnwanne <philemonnwanne@gmail.com>
2023-06-28 00:46:59 +0100

    deleted build dir

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,13 @@ import PlacesFormPage from "./pages/PlacesFormPage";
 import BookingPage from "./pages/BookingPage.jsx";
 import BookingsPage from "./pages/BookingsPage.jsx";
 
-axios.defaults.baseURL = import.meta.env.BACKEND_URL;
+// const pathy = process.env.REACT_APP_BACKEND_URL;
+// const pathy = import.meta.env.VITE_BACKEND_URL;
+// alert("this is a dev file");
+// alert(`the default path is: ${pathy}`);
+
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
+// axios.defaults.baseURL = process.env.REACT_APP_BACKEND_URL;
 axios.defaults.withCredentials = true;
 
 function App() {
