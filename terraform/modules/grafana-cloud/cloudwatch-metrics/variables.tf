4a1e19453e8fcb6d92bb6a29b9bd8675e68ebc77
philemonnwanne <philemonnwanne@gmail.com>
2023-07-05 21:16:10 +0100

    add grafana-cloud 🌤️

---
+++ terraform/modules/grafana-cloud/cloudwatch-metrics/variables.tf
@@ -0,0 +1,15 @@
+variable "external_id" {
+  type        = string
+  description = "This is your Grafana Cloud identifier and is used for security purposes."
+  validation {
+    condition     = length(var.external_id) > 0
+    error_message = "ExternalID is required."
+  }
+  default = ""
+}
+
+variable "iam_role_name" {
+  type        = string
+  description = "Customize the name of the IAM role used by Grafana for the CloudWatch integration."
+  default     = "GrafanaLabsCloudWatchIntegration"
+}
