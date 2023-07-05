c5fc9190a1d43e8f00059cb7466ca8c7f1f77978
philemonnwanne <philemonnwanne@gmail.com>
2023-07-05 22:33:23 +0100

    aupdate dev env 🏕️

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -1,15 +1,15 @@
-module "vpc" {
-  source = "../../modules/vpc"
-}
-
-module "security" {
-  source = "../../modules/security"
+module "alb" {
+  source = "../../modules/alb"
 
   vpc_id = module.vpc.vpc_id
+  # domain = module.route53.route53_zone_name
+  subnets         = module.vpc.vpc_public_subnet_id
+  security_groups = module.security.alb_security_group_id[*]
+  # backend_target = module.ecs.backend_task_id
 }
 
-module "grafana-cloud" {
-  source = "../../modules/grafana-cloud"
+module "cloudfront" {
+  source = "../../modules/cloudfront"
 }
 
 module "ecs" {
@@ -24,6 +24,10 @@ module "ecs" {
   target_group_arn = module.alb.target_group_arn
 }
 
+module "grafana-cloud" {
+  source = "../../modules/grafana-cloud"
+}
+
 # terraform {
 #   backend "s3" {
 #     # Replace this with your bucket name!
@@ -34,12 +38,6 @@ module "ecs" {
 #   }
 # }
 
-module "cloudfront" {
-  source = "../../modules/cloudfront"
-
-  # domain_name = module.alb.alb_dns
-}
-
 module "route53" {
   source = "../../modules/route53"
   cloudfront_alias_name = module.cloudfront.vacation_vibe_cloudfront_domain_name
@@ -48,14 +46,14 @@ module "route53" {
   alb_alias_zone_id = module.alb.alb_zone_id
 }
 
-module "alb" {
-  source = "../../modules/alb"
+module "security" {
+  source = "../../modules/security"
 
   vpc_id = module.vpc.vpc_id
-  # domain = module.route53.route53_zone_name
-  subnets         = module.vpc.vpc_public_subnet_id
-  security_groups = module.security.alb_security_group_id[*]
-  # backend_target = module.ecs.backend_task_id
+}
+
+module "vpc" {
+  source = "../../modules/vpc"
 }
 
 # module "state" {
