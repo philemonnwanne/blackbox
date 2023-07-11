21d1d482021cb82431bb057beb63250710afc514
philemonnwanne <philemonnwanne@gmail.com>
2023-07-11 03:44:03 +0100

    ----

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use(cookieParser());
 app.use(function (req, res, next) {
   res.header(
     "Access-Control-Allow-Origin",
-    "http://127.0.0.1:5173"
+    "https://frontend.philemonnwanne.me"
   );
   res.setHeader(
     "Access-Control-Allow-Credentials",
