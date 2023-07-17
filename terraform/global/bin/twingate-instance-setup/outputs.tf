2ec539eea116fb0474df24e6a603df7fdc4adbf1
philemonnwanne <philemonnwanne@gmail.com>
2023-07-17 12:19:57 +0100

    move twingate-ec2 dir to bin dir 👣

---
+++ terraform/global/bin/twingate-instance-setup/outputs.tf
@@ -0,0 +1,9 @@
+# output "access_token" {
+#   description = "connector access token"
+#   value = twingate_connector_tokens.tripvibe_connector_tokens.access_token
+# }
+
+# output "refresh_token" {
+#   description = "connector refresh token"
+#   value = twingate_connector_tokens.tripvibe_connector_tokens.refresh_token
+# }
