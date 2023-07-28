5b03ded5d0002eb4a96bcdfd46fb536cbf00b30c
philemonnwanne <philemonnwanne@gmail.com>
2023-07-28 15:07:35 +0100

    sort module dependency order ✅

--- terraform/test/unit/alb/maint.tf
+++ terraform/test/unit/alb/maint.tf
@@ -1,10 +1,9 @@
 module "alb" {
   source = "../../../modules/alb"
 
-  vpc_id = module.vpc.vpc_id
-
-  subnets         = module.vpc.vpc_public_subnet_id
   security_groups = module.security.alb_security_group_id[*]
+  subnets         = module.vpc.vpc_public_subnet_id
+  vpc_id = module.vpc.vpc_id
   # backend_target = module.ecs.backend_task_id
 }
 
@@ -13,12 +12,12 @@ output "alb_arn" {
   value = module.alb.alb_arn
 }
 
-module "vpc" {
-  source = "../../../modules/vpc"
-}
-
 module "security" {
   source = "../../../modules/security"
 
   vpc_id = module.vpc.vpc_id
+}
+
+module "vpc" {
+  source = "../../../modules/vpc"
 }
