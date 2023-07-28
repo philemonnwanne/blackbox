710f4826fd5682fbe0b47acbda608520dfb6d871
philemonnwanne <philemonnwanne@gmail.com>
2023-07-28 12:41:36 +0100

    update main.tf

--- terraform/env/dev/main.tf
+++ terraform/env/dev/main.tf
@@ -1,32 +1,32 @@
-# module "alb" {
-#   source = "../../modules/alb"
+module "alb" {
+  source = "../../modules/alb"
 
-#   vpc_id = module.vpc.vpc_id
-#   # domain = module.route53.route53_zone_name
-#   subnets         = module.vpc.vpc_public_subnet_id
-#   security_groups = module.security.alb_security_group_id[*]
-#   # backend_target = module.ecs.backend_task_id
-# }
+  vpc_id = module.vpc.vpc_id
+  # domain = module.route53.route53_zone_name
+  subnets         = module.vpc.vpc_public_subnet_id
+  security_groups = module.security.alb_security_group_id[*]
+  # backend_target = module.ecs.backend_task_id
+}
 
-# module "cloudfront" {
-#   source = "../../modules/cloudfront"
+module "cloudfront" {
+  source = "../../modules/cloudfront"
 
-#   resources      = ["${module.s3.cloudfront_s3_arn}/*"]
-#   s3_domain_name = module.s3.cloudfront_s3_domain_name
-# }
+  resources      = ["${module.s3.cloudfront_s3_arn}/*"]
+  s3_domain_name = module.s3.cloudfront_s3_domain_name
+}
 
-# module "ecs" {
-#   source = "../../modules/ecs"
+module "ecs" {
+  source = "../../modules/ecs"
 
-#   security_groups = module.security.backend_security_group_id[*]
-#   # subnet_ids = module.vpc.vpc_private_subnet_id
-#   subnet_ids = module.vpc.vpc_public_subnet_id
-#   # tags = local.tags
-#   vpc_id = module.vpc.vpc_id
-#   # target_group_arn = "${module.alb.target_group_arns[0]}"
-#   target_group_arn = module.alb.target_group_arn
-#   s3_bucket        = module.s3.backend_s3_bucket_name
-# }
+  security_groups = module.security.backend_security_group_id[*]
+  # subnet_ids = module.vpc.vpc_private_subnet_id
+  subnet_ids = module.vpc.vpc_public_subnet_id
+  # tags = local.tags
+  vpc_id = module.vpc.vpc_id
+  # target_group_arn = "${module.alb.target_group_arns[0]}"
+  target_group_arn = module.alb.target_group_arn
+  s3_bucket        = module.s3.backend_s3_bucket_name
+}
 
 # # module "grafana-cloud" {
 # #   source = "../../modules/grafana-cloud"
@@ -58,19 +58,19 @@
 # #     }
 # # }
 
-# module "route53" {
-#   source                   = "../../modules/route53"
-#   cloudfront_alias_name    = module.cloudfront.cloudfront_domain_name
-#   cloudfront_alias_zone_id = module.cloudfront.cloudfront_hosted_zone_id
-#   alb_alias_name           = module.alb.alb_dns
-#   alb_alias_zone_id        = module.alb.alb_zone_id
-# }
+module "route53" {
+  source                   = "../../modules/route53"
+  cloudfront_alias_name    = module.cloudfront.cloudfront_domain_name
+  cloudfront_alias_zone_id = module.cloudfront.cloudfront_hosted_zone_id
+  alb_alias_name           = module.alb.alb_dns
+  alb_alias_zone_id        = module.alb.alb_zone_id
+}
 
-# module "security" {
-#   source = "../../modules/security"
+module "security" {
+  source = "../../modules/security"
 
-#   vpc_id = module.vpc.vpc_id
-# }
+  vpc_id = module.vpc.vpc_id
+}
 
 module "vpc" {
   source = "../../modules/vpc"
@@ -80,11 +80,11 @@ module "vpc" {
 # #   source = "../../global/statefile"
 # # }
 
-# module "s3" {
-#   source = "../../modules/s3"
+module "s3" {
+  source = "../../modules/s3"
 
-#   policy = module.cloudfront.tripvibe_s3_policy
-# }
+  policy = module.cloudfront.tripvibe_s3_policy
+}
 
 module "twingate" {
   source = "../../modules/twingate"
